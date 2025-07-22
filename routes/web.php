d03860f8220b92f2962faa30195579d6d6743e9b
nasima-4755 <akter15-4755@diu.edu.bd>
2025-07-22 17:28:33 +0600

    database and admin with backend add

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\Controller;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::post('/login', [AuthenticationController::class, 'login']);
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/admin/dashboard', [Controller::class, 'dashboard']);
+});
+
+
+// Registration Routes
+Route::get('/registration', function () {
+    return view('auth.registration');
+})->name('registration');
+
+Route::post('/registration', [AuthenticationController::class, 'register']);
